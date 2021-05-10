76aae1ae40fc4591ff32f2f9688c1097eebfcfb2
Sasi3 <sasijagga@gmail.com>
2021-05-10 15:21:39 +0530

    Fixed indentation

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const countdown = () => {
-    const data = new Date("May 31, 2021 00:00:00").getTime();
-    const now = new Date().getTime();
-    const gap = data-now;
+  const data = new Date("May 31, 2021 00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = data - now;
 
-    const second = 1000;
-    const minute = second*60;
-    const hour = minute*60;
-    const day = hour*24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
 
-    document.querySelector(".day").innerText = textDay;
-    document.querySelector(".hour").innerText = textHour;
-    document.querySelector(".minutes").innerText = textMinute;
-    document.querySelector(".seconds").innerText = textSecond;
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".minutes").innerText = textMinute;
+  document.querySelector(".seconds").innerText = textSecond;
 };
 
 setInterval(countdown, 1000);
